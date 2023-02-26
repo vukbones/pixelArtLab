9082835ce756ea94fc5363367265c425a3b69f55
vukbones <vucossaa@gmail.com>
2023-02-26 16:34:04 +0100

    Added some js logic of coloring divs
    Now you can paint divs black by just click and dragging the segment which you want to color

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const container = document.getElementById("container");
 
+let isMouseDown = false;
+
+function change_color(item){
+    if(item.classList.contains("black")){
+        
+    }
+    else{
+        item.classList.add("black");
+    }
+}
+
 for(let i = 0; i < 256; i++){
     const item = document.createElement("div");
     item.classList.add("item");
     container.appendChild(item);
+    item.addEventListener("mousedown", ()=>{
+        isMouseDown = true;
+        change_color(item);
+    });
+    item.addEventListener("mouseover", ()=>{
+        if (isMouseDown){
+            change_color(item);
+        }
+    });
 }
+
+document.addEventListener("mouseup", ()=>{
+    isMouseDown = false;
+})
+
+
+
+
